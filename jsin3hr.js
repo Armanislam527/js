5a385e4de082cf23fd9b363a1b4da455542fa372
arman islam <armanislam527@gmail.com>
2024-12-14 10:43:27 +0600

    first comnit in this repo

---
+++ jsin3hr.js
@@ -0,0 +1,32 @@
+//alert("welcome to js learning ")//this line is for a alert message
+//this is a inline comment
+/* this is
+a multiline 
+comment*/
+var number = 9;
+/*dat type in js:undefined,null ,string,symbol,number and object
+there have three ways to declare variable*/
+
+var myname ="arman" //global
+myname = 8;
+let name="jscode"//local
+const pi=3.14 //unchangable
+var a ;
+var b =2;
+a=7;
+b=a;
+console.log(b)
+//var initializatiion
+var a =5 ;
+var b=10;
+var c= "i am a"
+//DO not change below this line
+a =a+ 1;
+b= b+ 5;//semicolon is not necessary
+c = c + "String"
+console.log(b)
+var mystr="I am a \"double quoted \" string inside \"double quote\""
+console.log(mystr)
+var str=`'<a href="htttp://www.example.com" target="_blank">link</a>'`
+console.log(str)
+console.log("First Line\nSecond Line\twith a tab\rCarriage return\bBackspace\fForm feed");
