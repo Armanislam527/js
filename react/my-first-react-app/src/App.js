f29b1865144962be7ab0bfbdc9b0fcb4b5cb219c
Arman Islam <armanislam527@gmail.com>
2025-05-06 00:03:17 +0600

    Add initial setup for React app with components, styles, and assets
    - Created main application structure with App.js, App.css, and index.js.
    - Added user profile component with conditional rendering and greeting.
    - Included AboutPage and MyButton components for additional functionality.
    - Implemented CSS styles for layout and animations.
    - Added logo and manifest for PWA support.
    - Included robots.txt for search engine directives.
    - Set up testing environment with initial test cases.

---
+++ react/my-first-react-app/src/App.js
@@ -0,0 +1,100 @@
+import logo from "./logo.svg";
+import "./App.css";
+const user = {
+	name: "Arman Islam",
+	imageUrl: "https://cv-of-arman.netlify.app/arman_office.jpeg",
+	imageSize: 90,
+};
+let content;
+let isLoggedIn = true;
+function GetGreeting() {
+	if (isLoggedIn) {
+		content = <h1>Welcome back!</h1>;
+	} else {
+		content = <h1>Please sign up.</h1>;
+	}
+	return content;
+}
+
+function Profile() {
+	return (
+		<>
+			<h1>{user.name}</h1>
+			<img
+				className="avatar"
+				src={user.imageUrl}
+				alt={"Photo of " + user.name}
+				style={{
+					width: user.imageSize,
+					// height: user.imageSize,
+					borderRadius: user.imageSize / 2,
+				}}
+			/>
+		</>
+	);
+}
+// import logo from './logo.svg';
+function AboutPage() {
+	return (
+		<>
+			<h1>About</h1>
+			<p>
+				Hello there.
+				<br />
+				How do you do?
+			</p>
+		</>
+	);
+}
+function ConditionalRendering() {
+	return isLoggedIn ? (
+		<div>
+			<h1>Statement is true.</h1>
+		</div>
+	) : (
+		<div>
+			<h1>Statement is false.</h1>
+		</div>
+	);
+	{
+		is;
+	}
+}
+function MyButton() {
+	return <button>I'm a button</button>;
+}
+
+// export default function MyApp() {
+//   return (
+//     <div>
+//       <h1>Welcome to my app</h1>
+//       <MyButton />
+//     </div>
+//   );
+// }
+export default function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				{/* <p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer">
+					Learn React
+				</a> */}
+				<Profile />
+				<GetGreeting />
+				<ConditionalRendering />
+				<MyButton />
+				<AboutPage />
+			</header>
+		</div>
+	);
+}
+
+// export default App;
